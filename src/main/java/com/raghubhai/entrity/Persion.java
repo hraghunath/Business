dc265ec9db98eb5eb5812fa4919baf208aa91074
RAJAJI <hraghunath08@gmail.com>
2024-05-02 19:04:51 +0530

    First commit

---
+++ src/main/java/com/raghubhai/entrity/Persion.java
@@ -0,0 +1,16 @@
+package com.raghubhai.entrity;
+
+import lombok.Data;
+
+@Data
+public class Persion {
+	
+	private int p_id;
+	private String name;
+	private String email;
+	private int age;
+	private long mobileNo;
+	
+	
+
+}
