8d9ddbde96ff809069d3b89cefba72b3cba19734
RAJAJI <hraghunath08@gmail.com>
2024-05-02 19:30:52 +0530

    Revertint the prgram changes "changed the Program"
    This reverts commit a8efb2a47180207750d3270712c407ad457161b9.

--- src/main/java/com/raghubhai/BusinessApplication.java
+++ src/main/java/com/raghubhai/BusinessApplication.java
@@ -8,12 +8,6 @@ public class BusinessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessApplication.class, args);
-		String str="raghunath";
-		String reverse="";
-		for(int i=str.length()-1;i>=0;i++){
-			reverse=reverse+str.charAt(i);
-		}
-		System.out.println("Revese of the string is::"+reverse);
 	}
 
 }
