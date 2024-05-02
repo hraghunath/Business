ca46414899e0324956bfc9702321473ebacf9187
RAJAJI <hraghunath08@gmail.com>
2024-05-02 19:45:27 +0530

    Add the latest varaible

--- src/main/java/com/raghubhai/BusinessApplication.java
+++ src/main/java/com/raghubhai/BusinessApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusinessApplication {
 
 	public static void main(String[] args) {
+
+		int a=10;
 		System.out.println("Tu hai ram tu hai rani tu hai ram");
+
 		SpringApplication.run(BusinessApplication.class, args);
 	}
 
