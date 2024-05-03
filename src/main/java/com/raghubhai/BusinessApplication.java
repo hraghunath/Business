88b4af88e93f33e1a0aeb5348e2b21365276b46f
RAGHUNATH HEMBRAM <117935585+hraghunath@users.noreply.github.com>
2024-05-03 06:42:15 +0530

    Update BusinessApplication.java

--- src/main/java/com/raghubhai/BusinessApplication.java
+++ src/main/java/com/raghubhai/BusinessApplication.java
@@ -13,6 +13,8 @@ public class BusinessApplication {
 
 		String s="Hii";
 
+		int i=123+345=2345;
+
 		System.out.println("Tu hai ram tu hai rani tu hai ram");
 
 		SpringApplication.run(BusinessApplication.class, args);
